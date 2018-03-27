bb46c2d63147fd37eaa83ee4749f0ec4dbb65374
Yoad <yoadwo@ac.sce.ac.il>
2018-03-27 12:45:55 +0300

    found bug: parseLine overwrites data

--- assignments/ex2/ex2.cpp
+++ assignments/ex2/ex2.cpp
@@ -10,6 +10,8 @@
 #include <regex>
 #include <cstdlib>  
 #include <algorithm>
+#include <unistd.h>
+#include <sys/wait.h>
 
 
 using namespace std;
@@ -17,29 +19,40 @@ using namespace std;
 /*  function ParseLine: parse string by delimiter
     input: line to parse (string::line) and delimiter to parse by (char::delimiter)
 */
-vector<string> parseLine(string line, char delimiter){
+vector<char*> parseLine(string line, char delimiter){
     // int n_spaces = count(line.begin(), line.end(), ' ');
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream tokenStream(line);
-    while (std::getline(tokenStream, token, delimiter))
+    vector<string> tokens;
+    string token;
+    istringstream tokenStream(line);
+    vector<char*> commandVector;
+
+    while (getline(tokenStream, token, delimiter))
     {
-        tokens.push_back(token);
+        //tokens.push_back(token);
+        commandVector.push_back(const_cast<char*>(token.c_str()));
     }
-    return tokens;
+    //added later, to make it execvp-compatible
+    commandVector.push_back(nullptr);
+
+    // charVec is one element bigger than tokens, to include 'nullptr'
+    //vector<const char*> charVec(tokens.size()+1,nullptr);
+    vector<const char*> charVec(commandVector.size()+1,nullptr);
+    /* for (size_t i=0; i<tokens.size();i++) {
+        charVec[i]= tokens[i].c_str();
+    } */
+    for (size_t i=0; i<commandVector.size();i++) {
+        charVec[i]= commandVector[i];
+    }
+
+    
+    // execvp (commandVector[0], &commandVector[0]);
+    execvp (charVec[0], charVec.data());
+    return commandVector;
+    
+    //return tokens;
 }
 
-/* function printPrompt: show shell messages
-    OSHell: <path> $
-*/
-void printPrompt(){
-    char cwd[1024];
-    std::cout << "OS SHell: "; 
-    if (getcwd(cwd, sizeof(cwd)) == NULL)
-        perror("??");
-    else
-        std:: cout << cwd << " >";
-}
+
 
 /* function handleIOErrors: handle getline() errors
     mainly used to catch End-Of-File (Ctrl+D)
@@ -100,6 +113,69 @@ string expandEnv(string text){
     return s;
 }
 
+<<<<<<< HEAD
+/* function expandStatus: replace '$?' with last exit status
+    input: user command (string: text) and last process exit status (int: exitStatus) 
+    will search for '$?' using regular expression until no more matches found
+    for each match, reconstruct string with some int (0..)
+*/
+=======
+/* 
+function expand tilde expands cwd and replace home directory with tilde
+*/
+string expandTildePrompt(string text){ 
+    static const regex env_re{R"(\/home\/[_a-zA-Z][_a-zA-Z0-9]*\/)"};
+    string s = text, user_home,s_temp, prefi, suffi;
+    smatch m; // <-- regular expression match object
+
+    while (regex_search(s, m, env_re)) // <-- use it here to get the match
+    {
+        // regex_search returns a match $VAR into variable "m"
+        user_home = m[0];
+        suffi = m.suffix().str();
+        // s_temp = <old string prefix> + <$VAR value> + <old string suffix>
+        s_temp ="~/";
+        // 
+        s_temp.append(suffi);
+       
+        // push back into s
+        s = s_temp;
+    }
+    
+    return s;
+}
+
+/* 
+function expand tilde expands cwd and replace tilde with home directory
+*/
+string expandTildeInput(string text){ 
+    static const regex env_re{R"(\~)"};
+    char *user_home;
+    string s = text, tilde,s_temp, prefi, suffi;
+    smatch m; // <-- regular expression match object
+
+    while (regex_search(s, m, env_re)) // <-- use it here to get the match
+    {
+        // regex_search returns a match $VAR into variable "m"
+        tilde = m[0];
+        // transform $VAR into VAR and lookup in env
+        user_home = getenv("HOME");
+        prefi = m.prefix().str();
+        suffi = m.suffix().str();
+        // s_temp = <old string prefix> + <$VAR value> + <old string suffix>
+        s_temp = prefi;
+        
+        s_temp.append(user_home);
+
+        s_temp.append(suffi);
+        // push back into s
+        s = s_temp;
+    }
+    
+    return s;
+}
+
+>>>>>>> c0692a674a11e4d9fd82c3b409dec53d52f375ae
 string expandStatus(string text, int exitStatus){
     static const regex env_re{R"(\$\?)"};
     string s = text, var,s_temp, prefi, suffi;
@@ -135,13 +211,26 @@ string expandStatus(string text, int exitStatus){
     return s;
 }
 
+/* function printPrompt: show shell messages
+    OSHell: <path> $
+*/
+void printPrompt(){
+    char cwd[1024];
+    std::cout << "OS SHell: "; 
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+        perror("??");
+    else
+        std:: cout << expandTildePrompt(cwd) << " >";
+}
+
 /* function changeDir: change current working directory
     input: array (vector:: res) that hold paramters to "cd" command
     prompt user if input is missing parameters
 */
-bool changeDir(vector<string> res){ 
+bool changeDir(vector<char*> res){ 
     if (res.size() > 1){
-        if (chdir(res[1].c_str())==0)
+        //if (chdir(res[1].c_str())==0)
+        if (chdir(res[1])==0)
             return true;
         else{
             cout << "cd: No such file or directory\n";
@@ -155,13 +244,37 @@ bool changeDir(vector<string> res){
     }
 }
 
+void execute(vector <char*> argv)
+{
+    pid_t pid;
+    int status;
+     
+    /* fork a child process           */
+    if ((pid = fork()) < 0) {     
+        cout << "*** ERROR: forking child process failed\n" << endl;
+        //exit(1);
+     }
+    /* for the child process:         */
+    else if (pid == 0) {          
+        if (execvp(argv[0], &argv[0]) < 0) {     /* execute the command  */
+            cout << "*** ERROR: exec failed\n" << endl;
+            exit(1);
+          }
+     }
+    /* for the parent:      */
+     else {                                  
+          while (wait(&status) != pid)       /* wait for completion  */
+               ;
+     }
+}
+
 int main(){
 
     std::cout <<"Welcome to OS SHell\n";
     
     int linelen, exitStatus = 0;
     string line;
-    vector<string> res;
+    vector<char*> res;
 
     printPrompt();    
     
@@ -173,6 +286,8 @@ int main(){
             printPrompt();
             continue;
         }
+        
+        line=expandTildeInput(line);
         line = expandEnv(line);
         line = expandStatus(line, exitStatus);
         res = parseLine(line, ' ');
@@ -180,19 +295,24 @@ int main(){
         if (res.empty()){
 
         }
-        else if (!res[0].compare("cd")){
+        //else if (!res[0].compare("cd")){
+        else if (!strncmp(res[0],"cd",strlen("cd"))){
             if (changeDir(res))
                 exitStatus = 0;
             else
                 exitStatus = 1;
         }
-        else if (!res[0].compare("exit"))
-        {
+        //else if (!res[0].compare("exit"))
+        else if (!strncmp(res[0],"exit",strlen("exit"))){
             std::cout << "C ya!\n";
             break;
         }
-        else
-            std::cout << res[0] <<": command not found\n";
+        else{
+            //    execute(res);
+            cout << res[0] <<": command not found.\n";
+
+        }
+        
 
         printPrompt();
 

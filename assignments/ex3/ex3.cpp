acd4bb1c38469b6bc879e090752fb0b41514b32d
Yoad <yoadwo@ac.sce.ac.il>
2018-04-10 17:59:00 +0300

    add ex3 hello world

---
+++ assignments/ex3/ex3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>  
+
+using namespace std;
+
+int main(){
+    cout << "hello task 3\n";
+
+    cout << "Press any key to continue...\n";
+    getchar();
+    return 0;
+}

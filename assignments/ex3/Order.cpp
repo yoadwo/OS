4029c44f6b6a3dff77b7dc753ca25399bc834e46
Unknown <yoadwo@ac.sce.ac.il>
2018-04-20 12:20:07 +0300

    order: from class to struct

--- assignments/ex3/Order.cpp
+++ assignments/ex3/Order.cpp
@@ -5,37 +5,77 @@
 #include "Order.h"
 using namespace std;
  
+struct foo {
+  int bar;
+  foo() : bar(3) {}   //look, a constructor
+  int getBar() 
+  { 
+    return bar; 
+  }
+};
 
-Order :: Order(int customerId, int itemId, int amount) 
-  : _customerId(customerId), _itemId (itemId), _amount(amount), _done(true)
-{
-}
- 
-int Order :: getCustomerId() 
-{
+typedef struct Order{
+    int _customerId, _itemId, _amount;
+    bool _done;
+
+    Order(int customerId, int itemId, int amount) 
+    : _customerId(customerId), _itemId (itemId), _amount(amount), _done(true)
+    {}
+
+    int getCustomerId() {
     return _customerId; 
-}	
-	
-int Order :: getItemId(){
-    return _itemId;
-}
-int Order :: getAmount(){
-    return _amount;
-}
-bool Order :: isDone(){
-    return _done;
-}
-void Order :: print()
-{
-    cout 
-    << "customerId: " << _customerId 
-    << ", itemId: " << _itemId
-    << ", amount: " << _amount
-    << ", Done: " << _done
-    << "\n";
-}
+    }
+
+    int getItemId(){
+        return _itemId;
+    }
+    int getAmount(){
+        return _amount;
+    }
+    bool isDone(){
+        return _done;
+    }
+    void  print(){
+        cout 
+        << "customerId: " << _customerId 
+        << ", itemId: " << _itemId
+        << ", amount: " << _amount
+        << ", Done: " << _done
+        << "\n";
+    }
+
+}  Order;
+
+// Order :: Order(int customerId, int itemId, int amount) 
+//   : _customerId(customerId), _itemId (itemId), _amount(amount), _done(true)
+// {
+// }
  
-bool Order :: equals(const Order &otherOrder)
-{
-    return true;
-}
+// int Order :: getCustomerId() 
+// {
+//     return _customerId; 
+// }	
+	
+// int Order :: getItemId(){
+//     return _itemId;
+// }
+// int Order :: getAmount(){
+//     return _amount;
+// }
+// bool Order :: isDone(){
+//     return _done;
+// }
+// void Order :: print()
+// {
+//     cout 
+//     << "customerId: " << _customerId 
+//     << ", itemId: " << _itemId
+//     << ", amount: " << _amount
+//     << ", Done: " << _done
+//     << "\n";
+// }
+ 
+// bool Order :: equals(const Order &otherOrder)
+// {
+//     return true;
+// }
--- assignments/ex3/Order.h
+++ assignments/ex3/Order.h
@@ -1,24 +1,24 @@
-#ifndef ORDER_H
-#define ORDER_H
+// #ifndef ORDER_H
+// #define ORDER_H
 
-using namespace std;
+// using namespace std;
 
-class Order
-{
-     private :
-          int _customerId, _itemId, _amount;
-          bool _done;
-     public :
-       //with default value
-       Order(int customerId, int itemId, int amount);
-       //getter functions
-       int getCustomerId();
-       int getItemId();
-       int getAmount();
-       bool isDone();
-       // Print a description of object
-       void print();
-       //compare two time object
-       bool equals(const Order&);
-};
-#endif
+// class Order
+// {
+//      private :
+//           int _customerId, _itemId, _amount;
+//           bool _done;
+//      public :
+//        //with default value
+//        Order(int customerId, int itemId, int amount);
+//        //getter functions
+//        int getCustomerId();
+//        int getItemId();
+//        int getAmount();
+//        bool isDone();
+//        // Print a description of object
+//        void print();
+//        //compare two time object
+//        bool equals(const Order&);
+// };
+// #endif

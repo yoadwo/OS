67e3e5a31bb3fa926ae6270e81b539d1d6b005f5
Yoad <yoadwo@ac.sce.ac.il>
2018-05-19 17:56:19 +0300

    Resolver

--- assignments/ex5/task2/ResolverPool.hpp
+++ assignments/ex5/task2/ResolverPool.hpp
@@ -3,9 +3,18 @@
 
 #include "ThreadPool.hpp"
 
+#include <fstream>
+
 class ResolverPool: public ThreadPool
 {
+    private:
+        ofstream m_OutputFile;
 
+    public:
+        ResolverPool(int poolsize, char *output, bool bLinger);
+        ~ResolverPool();
+        SafeQueue*  getTaskQueue();
+        bool isOutputOpen();
 };
 
 #endif
